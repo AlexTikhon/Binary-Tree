2211529764755e93c5051f728da077b729efc3e7
AlexTikhon <optimalyo@gmail.com>
2016-01-29 12:37:43 +0300

    Created the method 'insert'

--- binary-tree.js
+++ binary-tree.js
@@ -6,7 +6,35 @@ class BinaryTree {
 	}
 
 	insert(data) {
+		if (this.isEmpty()){
+			this.root = new Node(data);
+			return;
+		}
 
+		var currNode = this.root;
+
+		while (currNode) {
+
+			if (data < currNode.data) {
+				if (currNode.left == null) {
+					currNode.left = new Node(data);
+					break;
+				} else {
+					currNode = currNode.left;
+				}
+			}
+
+			if (data > currNode.data) {
+				if (currNode.right == null) {
+					currNode.right = new Node(data);
+					break;
+				} else {
+					currNode = currNode.right;
+				}
+			}
+
+			if (data == currNode.data) break;
+		}
 	}
 
 	contains(data) {
